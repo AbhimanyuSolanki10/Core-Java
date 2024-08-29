34211c7de7c53eadf0e6c1236a9502fb43e74e83
AbhimanyuSolanki10 <abhimanyusolanki10@gmail.com>
2024-08-30 00:02:56 +0530

    30-08-2024

--- While_Loop/Program23.java
+++ While_Loop/Program23.java
@@ -1,28 +1,30 @@
 
 import java.util.Scanner;
 
-class Program23 {
-
-    public static void main(String[] args) {
+class Program23{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n to Print n number of fibbonaci series ");
+        System.out.println("Enter the value ");
         int n = sc.nextInt();
-        getFibbonaci(n);
-    }
-
-    public static void getFibbonaci(int n) {
         int i = 1;
         int first = 0;
         int second = 1;
-        if(n<0){
-            System.out.println("Entered Negative Number");
+        int next ;
+        if(n==1){
+            System.out.print(first + " ");
+        }else if(n==2){
+            System.out.print(first + " " + second + " ");
+        }else{
+            System.out.print(first + " " + second + " ");
+            while(i<=n-2){
+            next = first + second;
+            System.out.print(next + " ");
+            first = second;
+            second = next;
+            i++;
+
+            }
+
         }
-        else if (n == 1 || n==0) {
-            System.out.println(0);
-        } 
-        else if (n == 2) {
-            System.out.print(0 + " " + i);
-        }
-        
     }
 }
